e3bdf2620050b9beea28dbb33e2d1e1836eb042b
sandeep-luhaniwal <sandeepluh0001.com>
2025-05-16 13:09:16 +0530

    first commit

---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,4 @@
+module.exports = (err, req, res, next) => {
+  const statusCode = res.statusCode || 500;
+  res.status(statusCode).json({ message: err.message });
+};
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const taskRoutes = require('./routes/taskRoutes');
+const errorMiddleware = require('./middleware/errorMiddleware');
+const cors = require('cors');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api/tasks', taskRoutes);
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
